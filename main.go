058d096e60aaf59b391ae4c448cf703565e6f40d
Takahiro Hatakeyama <t-hatakeyama@keyamin.jp>
2022-01-19 23:01:30 +0900

    [add] /bookmarkのPOSTの仮実装

--- main.go
+++ main.go
@@ -2,27 +2,50 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 type Bookmark struct {
+	Id  int    `json:"id"`
 	Url string `json:"url"`
 }
 
+var id int
+var inMemoryData []Bookmark
+
 func main() {
-	sampleBookmark := Bookmark{"https://youtube.com"}
-	http.HandleFunc("/bookmark", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Methods", "GET,POST")
+	var bookmark http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf8")
 		if r.Method == "GET" {
 			w.WriteHeader(http.StatusOK)
-			s, err := json.Marshal(sampleBookmark)
+			s, err := json.Marshal(inMemoryData)
 			if err != nil {
 				panic(err)
 			}
 			w.Write(s)
 		}
-	})
-
+		if r.Method == "POST" {
+			var bookmarkPostBody struct {
+				Url string `json:"url"`
+			}
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				panic(err)
+			}
+			if err := json.Unmarshal(body, &bookmarkPostBody); err != nil {
+				panic(err)
+			}
+			created := &Bookmark{id, bookmarkPostBody.Url}
+			inMemoryData = append(inMemoryData, *created)
+			w.Header().Set("Location", fmt.Sprintf("http://localhost:8080/bookmark/%d", created.Id))
+			w.WriteHeader(http.StatusCreated)
+			s, _ := json.Marshal(*created)
+			w.Write(s)
+			id++
+		}
+	}
+	http.HandleFunc("/bookmark", bookmark)
 	http.ListenAndServe(":8080", nil)
 }
